037ec0e18191e13ce0cd134e0f238fc0a116d7f6
tizihoxha <tizihoxha@gmail.com>
2022-12-06 01:16:23 -0800

    added

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -56,8 +56,8 @@ def update_state(state_id):
         abort(404)
     if not request.get_json():
         return make_response(jsonify({'error': 'Not a JSON'}), 400)
-    for key, value in request.get_json().items():
-        if key not in ['id', 'created_at', 'updated_at']:
-            setattr(state, key, value)
+    for k, v in request.get_json().items():
+        if k not in ['id', 'created_at', 'updated_at']:
+            setattr(state, k, v)
     state.save()
     return jsonify(state.to_dict())
