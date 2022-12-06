ea8fb9330f1b2db360986b2336c713d7380babe8
tizihoxha <tizihoxha@gmail.com>
2022-12-06 01:54:22 -0800

    added

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -45,7 +45,7 @@ def post_state():
         return make_response(jsonify({'error': 'Not a JSON'}), 400)
     if 'name' not in request.get_json():
         return make_response(jsonify({'error': 'Missing name'}), 400)
-    state = State(**request.get_json())
+    state = State(request.get_json())
     state.save()
     return make_response(jsonify(state.to_dict()), 201)
 
@@ -59,8 +59,8 @@ def put_state(state_id):
         abort(404)
     if not request.get_json():
         return make_response(jsonify({'error': 'Not a JSON'}), 400)
-    for key, val in request.get_json().items():
+    for key, value in request.get_json().items():
         if key not in ['id', 'created_at', 'updated_at']:
-            setattr(state, key, val)
+            setattr(state, key, value)
     state.save()
     return jsonify(state.to_dict())
