1a2113e242ae30b90648f43bcc3e41dc9e6c75a0
tizihoxha <tizihoxha@gmail.com>
2022-12-06 05:33:41 -0800

    added

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -53,13 +53,13 @@ def post_place(city_id):
         abort(404)
     if not request.get_json():
         return make_response(jsonify({'error': 'Not a JSON'}), 400)
-    kwargs = request.get_json()
-    if 'user_id' not in kwargs:
+    var = request.get_json()
+    if 'user_id' not in var:
         return make_response(jsonify({'error': 'Missing user_id'}), 400)
-    user = storage.get("User", kwargs['user_id'])
+    user = storage.get("User", var['user_id'])
     if user is None:
         abort(404)
-    if 'name' not in kwargs:
+    if 'name' not in var:
         return make_response(jsonify({'error': 'Missing name'}), 400)
     var['city_id'] = city_id
     place = Place(**var)
@@ -67,7 +67,7 @@ def post_place(city_id):
     return make_response(jsonify(place.to_dict()), 201)
 
 
-@app_views.route('/places/<string:place_id>', methods=['PUT'],
+@app_views.route('/places/<place_id>', methods=['PUT'],
                  strict_slashes=False)
 def put_place(place_id):
     """updating a place"""
