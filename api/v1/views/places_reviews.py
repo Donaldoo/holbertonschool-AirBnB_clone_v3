be535ad76521a7f985c5024d6e89be3745a90a3a
tizihoxha <tizihoxha@gmail.com>
2022-12-07 02:11:45 -0800

    added

--- api/v1/views/places_reviews.py
+++ api/v1/views/places_reviews.py
@@ -78,7 +78,7 @@ def put_review(review_id):
         return make_response(jsonify({'error': 'Not a JSON'}), 400)
     for key, value in request.get_json().items():
         if key not in ['id', 'user_id', 'place_id',
-                        'created_at', 'updated_at']:
+                       'created_at', 'updated_at']:
             setattr(review, key, value)
     review.save()
     return jsonify(review.to_dict())
